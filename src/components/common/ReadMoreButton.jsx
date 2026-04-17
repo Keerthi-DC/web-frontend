60cb063cd12d3a0eca0abbfd43e26d18736af3f7
Keerthi-DC <dckeerthi2004@gmail.com>
2026-04-18 01:20:52 +0530

    chore: remove unused common directory

--- src/components/common/ReadMoreButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const ReadMoreButton = ({ to, text = 'Read More' }) => (
-  <Link to={to} className="inline-block text-sm text-yellow-300 hover:underline mt-2">{text}</Link>
-);
-
-export default ReadMoreButton;
